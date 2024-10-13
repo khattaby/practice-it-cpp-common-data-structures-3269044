3bb4518e1b39d13866fcbccd5917b66f1f9bd37f
khattaby <62982184+khattaby@users.noreply.github.com>
2024-10-13 11:04:35 +0000

    sum-array-sol

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    //...TODO: implement
+    if (size == 0)
+    {
+        return 0;
+    }
+    if (size == 1)
+    {
+        return arr[0];
+    }
+
+    int sum = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return sum ; 
 }
 
 int main()
